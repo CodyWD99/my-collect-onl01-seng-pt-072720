67f0ce1d7ef29fcd488052ab934ade6f5845c85b
Cody Dupuis <codydupuis99@gmail.com>
2020-08-03 06:43:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,11 +4,11 @@ def my_collect(array)
   counter = 0
   new_array = []
    while counter < array.length
-   yield(array[counter])
+   new_array << yield(array[counter])
    counter+=1 
    
  end
- 
+ new_array
 end
  
  my_collect([]) {|blank| "#{blank}"}
